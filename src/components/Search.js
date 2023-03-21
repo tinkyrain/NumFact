9104716a0eb012770cdcca4a02a484ccca3acd97
vladislow <gray849023@gmail.com>
2023-03-21 14:00:05 +0500

    add query

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,25 @@
 import React from "react";
-
-
 import './Search.css'
 
 const Search = () => {
+
+    const URL = 'http://numbersapi.com/';
+
+    const getInfo = () => {
+        fetch(`${URL}${getRandomInt(100)}`)
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+        })
+    }
+
+    const getRandomInt = (max) => {
+        return Math.floor(Math.random() * max);
+    }
+
     return(
         <section className='searchSection'>
-            <button className="btn">Numerical Fact</button>
+            <button className="btn" onClick={() => getInfo()}>Numerical Fact</button>
         </section>
     );
 }
